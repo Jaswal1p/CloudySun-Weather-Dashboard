ffb655578470dfd09b2a3812318e8336052bb383
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-21 20:25:49 -0400

    added code for cities updateSearch history

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,34 @@ const callOpenWeather = (city) => {
 }
 })   
 }
+
+
+const updateSearchHistory = () => {
+    // Pulls localStorage results of previous searches
+    previousSearch = JSON.parse(localStorage.getItem("searches"));
+
+    // Declared under function to ensure list is updated each time
+    const existingButtons = document.querySelectorAll("#previous-searches button");
+
+    if (previousSearch !== null) {
+        existingButtons.forEach(button => {
+            // Ensures buttons aren't repeated for existing searches
+            for (let i = 0; i < previousSearch.length; i++)
+            if (button.dataset.city.includes(previousSearch[i])) {
+                previousSearch.splice(i, i + 1);
+            }
+        })
+        for (let i = 0; i < previousSearch.length; i++) {
+            const searchButton = document.createElement("button");
+            searchButton.classList.add("m-2", "btn", "btn-light");
+            // Sets data-city attribute on button for event listener to reference
+            searchButton.dataset.city = previousSearch[i];
+            searchButton.textContent = previousSearch[i];
+            searchButton.addEventListener("click", (event) => {
+                // References data-city property to call API
+                callOpenWeather(event.target.dataset.city);
+            })
+            previousSearchContainer.appendChild(searchButton); 
+        }
+    }
+}
