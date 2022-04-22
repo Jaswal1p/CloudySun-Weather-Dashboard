7dc4d3083b746d91ebe7e65e86d5c0a30d11e308
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-21 20:19:59 -0400

    added code for all declarations in the script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const cityNameInput = document.querySelector("#city-name");
+const searchForm = document.querySelector("#search-form");
+const currentConditionsUl = document.querySelector("#current-forecast #conditions");
+const currentConditionsH3 = document.querySelector("#current-forecast h3");
+const previousSearches = document.querySelector("#previous-searches");
+const previousSearchContainer = document.querySelector("#previous-searches .card-body");
+const dailyCardContainer = document.querySelector("#daily-forecast");
+const fiveDayHeader = document.querySelector("#five-day");
+
+// Declares localCityArray in global variable
+const localCityArray = [];
